438c5406a291490876a7151084b32124f0678443
BadLuckZ <thanagorn8802@gmail.com>
2025-02-23 22:36:28 +0700

    fix: bring the sidebar to the front

--- src/components/homepage/navbar.tsx
+++ src/components/homepage/navbar.tsx
@@ -4,8 +4,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 
-import { getImageURL } from '@/utils/image'
-
 interface MenuItem {
   title: string
   link: string
@@ -25,63 +23,57 @@ const Navbar: React.FC = () => {
 
   return (
     <div className='relative'>
-      <header className='z-50 flex w-full flex-col'>
+      <header className='z-10 flex w-full flex-col'>
+        {/* Burger's Content */}
+        <div
+          className={`absolute left-0 z-20 w-full bg-[#076855] transition-all duration-500 ease-in-out ${
+            isMenuOpen ? 'translate-x-[0px]' : '-translate-x-full'
+          }`}
+        >
+          {/* X Button */}
+          <div className='flex w-full justify-end px-[41px] py-[36px]'>
+            <button type='button' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <div className='relative grid gap-1.5'>
+                <span className='h-0.5 w-6 translate-y-1 rotate-45 rounded-full bg-white' />
+                <span className='h-0.5 w-6 -translate-y-1 -rotate-45 rounded-full bg-white' />
+              </div>
+            </button>
+          </div>
+
+          {/* Content */}
+          <ul className='flex flex-col items-center justify-center'>
+            {menuItems.map((item) => (
+              <Link
+                key={item.title}
+                className='w-full cursor-pointer py-[36px] pl-[41px] text-xl font-semibold text-white hover:bg-[#064E41]'
+                href={item.link}
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </ul>
+        </div>
+
         <div className='flex h-[72px] w-full items-center justify-between bg-[#064E41] px-2 py-1'>
           {/* Logo is at the left */}
           <Image
             alt='logo'
             height={64}
-            src={getImageURL('/homepage/oph_logo-01.svg')}
+            src='/homepage/oph_logo-01.svg'
             width={64}
           />
+
           {/* Burger Button is at the right */}
-          <button
-            className='group'
-            type='button'
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
+          <button type='button' onClick={() => setIsMenuOpen(!isMenuOpen)}>
             <div className='relative grid justify-items-center gap-1.5'>
-              <span
-                className={`h-0.5 w-6 rounded-full bg-white transition-transform duration-300 ${
-                  isMenuOpen ? 'translate-y-2 rotate-45' : ''
-                }`}
-              />
-              <span
-                className={`h-0.5 w-6 rounded-full bg-white transition-transform duration-300 ${
-                  isMenuOpen ? 'scale-x-0' : ''
-                }`}
-              />
-              <span
-                className={`h-0.5 w-6 rounded-full bg-white transition-transform duration-300 ${
-                  isMenuOpen ? '-translate-y-2 -rotate-45' : ''
-                }`}
-              />
+              <span className='h-0.5 w-6 rounded-full bg-white' />
+              <span className='h-0.5 w-6 rounded-full bg-white' />
+              <span className='h-0.5 w-6 rounded-full bg-white' />
             </div>
           </button>
         </div>
       </header>
-
-      {/* Burger's Content */}
-      <div
-        className={`absolute left-0 top-[72px] z-40 w-[245px] border-r bg-[#076855] transition-all duration-500 ease-in-out ${
-          isMenuOpen
-            ? 'translate-x-[0px] opacity-100'
-            : 'pointer-events-none -translate-x-full opacity-0'
-        }`}
-      >
-        <ul className='flex flex-col items-center justify-center'>
-          {menuItems.map((item) => (
-            <Link
-              key={item.title} // Simplified key since title should be unique
-              className='w-full cursor-pointer py-[36px] pl-[41px] text-xl font-semibold text-white hover:bg-[#064E41]'
-              href={item.link}
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </ul>
-      </div>
     </div>
   )
 }
