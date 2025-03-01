adf133b25412a36c12fa28ec8192643d5c6c6345
Amphi <6633287021@student.chula.ac.th>
2025-03-02 00:21:51 +0700

    fix: add bg flower

--- src/components/pick_your_flower/subpages/flower-result.tsx
+++ src/components/pick_your_flower/subpages/flower-result.tsx
@@ -39,34 +39,58 @@ const FlowerResult: React.FC<FlowerResultProps> = ({
 
   return (
     <div className='relative flex flex-col items-center gap-5 bg-gradient-to-b from-[#FFFFFF] to-[#EA88BD] px-6 py-10'>
-      <div className='font-cloud-soft text-2xl font-bold tracking-tight text-primary-green'>
+      <div className='absolute left-0 top-1/3 z-0'>
+        <Image
+          alt='background flower'
+          height={92}
+          src='/assets/pick_your_flower/flower-white-l.svg'
+          width={92}
+        />
+      </div>
+      <div className='absolute right-0 top-0 z-0'>
+        <Image
+          alt='background flower'
+          height={92}
+          src='/assets/pick_your_flower/flower-white-r.svg'
+          width={92}
+        />
+      </div>
+      <div className='absolute bottom-10 right-0 z-0'>
+        <Image
+          alt='background flower'
+          height={75}
+          src='/assets/pick_your_flower/flower-white-r.svg'
+          width={75}
+        />
+      </div>
+      <div className='z-10 font-cloud-soft text-2xl font-bold tracking-tight text-primary-green'>
         Your Flower
       </div>
-      <div className='relative flex items-center justify-center'>
-        <div className='absolute h-[383px] w-[217px] rounded-[18px] border-[1px] border-[#DD579B] blur-[2px]' />
+      <div className='relative z-10 flex items-center justify-center'>
+        <div className='absolute z-0 h-[383px] w-[217px] rounded-[18px] border-[1px] border-[#DD579B] blur-[2px]' />
         <Image
           alt='flower result'
-          className='relative'
+          className='relative z-10'
           height={370}
           src={resultImage}
           width={206}
         />
         <Image
           alt='flower with leaf'
-          className='absolute left-[-15px] top-[-20px]'
+          className='absolute left-[-15px] top-[-20px] z-10'
           height={50}
           src='/assets/pick_your_flower/flower-w-leaf.svg'
           width={50}
         />
         <Image
           alt='girl with flower'
-          className='absolute bottom-[-17px] right-[-45px]'
+          className='absolute bottom-[-17px] right-[-45px] z-10'
           height={104}
           src='/assets/pick_your_flower/flower-girl.svg'
           width={66}
         />
       </div>
-      <div className='mt-4 flex w-full items-center justify-center gap-2'>
+      <div className='z-10 mt-4 flex w-full items-center justify-center gap-2'>
         <button
           className='flex w-[143px] items-center justify-center gap-1 rounded-lg bg-[#076855] px-3 py-1'
           type='button'
@@ -98,7 +122,9 @@ const FlowerResult: React.FC<FlowerResultProps> = ({
           </div>
         </button>
       </div>
-      <InvitationCard />
+      <div className='z-10'>
+        <InvitationCard />
+      </div>
     </div>
   )
 }
--- src/components/pick_your_flower/subpages/select-card.tsx
+++ src/components/pick_your_flower/subpages/select-card.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import React from 'react'
 
 import FlowerCard from '@/components/pick_your_flower/flower-card'
 
 interface SelectFlowerProps {
   selectedCard: number | null
   cardList: string[]
-  handleClick: () => void
+  handleClick: (index: number) => void
 }
 
 const SelectCard: React.FC<SelectFlowerProps> = ({
@@ -14,8 +15,32 @@ const SelectCard: React.FC<SelectFlowerProps> = ({
   cardList,
 }) => {
   return (
-    <div className='flex h-auto flex-col items-center bg-gradient-to-b from-[#FAE9F3] to-[#DD579B] px-6 py-10'>
-      <div className='flex items-center justify-center gap-1'>
+    <div className='relative flex h-auto flex-col items-center bg-gradient-to-b from-[#FAE9F3] to-[#DD579B] px-6 py-10'>
+      <div className='absolute left-0 top-1/3 z-0'>
+        <Image
+          alt='background flower'
+          height={92}
+          src='/assets/pick_your_flower/flower-white-l.svg'
+          width={92}
+        />
+      </div>
+      <div className='absolute right-0 top-0 z-0'>
+        <Image
+          alt='background flower'
+          height={92}
+          src='/assets/pick_your_flower/flower-white-r.svg'
+          width={92}
+        />
+      </div>
+      <div className='absolute bottom-10 right-0 z-0'>
+        <Image
+          alt='background flower'
+          height={75}
+          src='/assets/pick_your_flower/flower-white-r.svg'
+          width={75}
+        />
+      </div>
+      <div className='relative z-10 flex items-center justify-center gap-1'>
         <Image
           alt='flower'
           height={25}
@@ -26,10 +51,10 @@ const SelectCard: React.FC<SelectFlowerProps> = ({
           เลือกการ์ดเพื่อเปิด
         </div>
       </div>
-      <div className='font-mitr text-base font-normal tracking-tight text-primary-green'>
+      <div className='relative z-10 font-mitr text-base font-normal tracking-tight text-primary-green'>
         Pick Your Card
       </div>
-      <div className='mb-16 mt-6 grid grid-cols-3 gap-3'>
+      <div className='relative z-10 mb-16 mt-6 grid grid-cols-3 gap-3'>
         {cardList.map((cardImage: string, index: number) => (
           <FlowerCard
             key={cardImage}
@@ -38,7 +63,7 @@ const SelectCard: React.FC<SelectFlowerProps> = ({
                 ? cardImage
                 : '/assets/pick_your_flower/flower-card-cover.png'
             }
-            onClick={handleClick}
+            onClick={() => handleClick(index)}
           />
         ))}
       </div>
