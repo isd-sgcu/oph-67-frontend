7428e2b7f9601a90a1a8dc85dc4af3e448eed047
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-03-05 15:54:06 +0700

    fix: parsed env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 }
 
 export default nextConfig
