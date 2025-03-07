19f9c1be6b825ae80e52eff30e5d29c7d5549ff5
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-03-07 14:15:19 +0700

    feat: add `sidebar.tsx`

--- src/components/homepage/navbar.tsx
+++ src/components/homepage/navbar.tsx
@@ -3,49 +3,21 @@
 import Link from 'next/link'
 import { useState } from 'react'
 
-import { NavbarItems } from '@/const/navbar'
+import Sidebar from '@/components/homepage/sidebar'
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false)
   // const isRegistered = false
 
+  const toggleSidebar = (): void => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div>
       <header className='z-10 flex w-full flex-col'>
-        {/* Burger's Content */}
-        <div
-          className={`duration-[500ms] absolute left-0 z-20 w-full bg-[#076855] transition-all ease-in-out ${isOpen ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'}`}
-        >
-          {/* X Button */}
-          <div
-            className={`flex w-full justify-end px-[41px] py-[36px] transition-opacity duration-100 ${
-              isOpen ? 'opacity-100' : 'opacity-0'
-            }`}
-          >
-            <button type='button' onClick={() => setIsOpen(!isOpen)}>
-              <div className='relative grid gap-1.5'>
-                <span className='h-0.5 w-6 translate-y-1 rotate-45 rounded-full bg-white' />
-                <span className='h-0.5 w-6 -translate-y-1 -rotate-45 rounded-full bg-white' />
-              </div>
-            </button>
-          </div>
-
-          {/* Content */}
-          <ul
-            className={`flex flex-col items-center justify-center transition-opacity duration-200 ${isOpen ? 'opacity-100' : 'opacity-0'}`}
-          >
-            {NavbarItems.map((item) => (
-              <Link
-                key={item.title}
-                className='w-full cursor-pointer py-[36px] pl-[41px] font-anuphan text-xl font-semibold text-white hover:bg-[#064E41]'
-                href={item.link}
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                {item.title}
-              </Link>
-            ))}
-          </ul>
-        </div>
+        {/* Sidebar Component */}
+        <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
 
         <div className='flex h-[72px] w-full items-center justify-between bg-[#064E41] py-1 pl-2 pr-4'>
           {/* Logo is at the left */}
@@ -67,8 +39,7 @@ const Navbar: React.FC = () => {
                 backgroundImage: 'url(/assets/homepage/profile.svg)',
               }}
             />
-            {/* )} */}
-            <button type='button' onClick={() => setIsOpen(!isOpen)}>
+            <button type='button' onClick={toggleSidebar}>
               <div className='relative grid justify-items-center gap-1.5'>
                 <span className='h-0.5 w-6 rounded-full bg-white' />
                 <span className='h-0.5 w-6 rounded-full bg-white' />
