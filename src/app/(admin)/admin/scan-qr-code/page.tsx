d19455be7fcb4a51f022ed74808d163979455320
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-03-07 21:57:42 +0700

    feat: implement QR code scanning

--- src/app/(admin)/admin/scan-qr-code/page.tsx
+++ src/app/(admin)/admin/scan-qr-code/page.tsx
@@ -1,25 +1,46 @@
+'use client'
+
+import { EditSolid } from '@mynaui/icons-react'
 import Image from 'next/image'
+import Link from 'next/link'
+
+import QrButton from '@/components/scan-qr-code/qr-button'
+import { Button } from '@/components/ui/button'
 
 const ScanQrCodePage: React.FC = () => {
   return (
-    <div className='flex h-screen flex-col items-center bg-gradient-to-br from-[#DD579B] via-[#F2AFD4] to-[#ECF3C0] px-8 py-10'>
-      <div className='relative flex flex-col items-center'>
-        <div className='absolute left-1/2 top-1/2 z-0 h-[89px] w-[97px] -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white blur-xl' />
-        <Image
-          alt='oph logo'
-          className='z-10'
-          height={89}
-          src='/assets/scan_qr_code/oph-logo.png'
-          width={97}
-        />
-      </div>
-      <div className='mt-2 font-ubuntu text-base font-normal text-white'>
-        CHULA OPENHOUSE 2025
-      </div>
-      <div className='mt-8 flex flex-col items-center gap-1 font-ubuntu'>
-        <div className='text-2xl font-normal text-white'>Welcome!, Staff</div>
-        <div className='text-base font-normal text-white'>username</div>
-      </div>
+    <div className='flex h-screen flex-col items-center justify-between bg-gradient-to-br from-[#DD579B] via-[#F2AFD4] to-[#ECF3C0] px-8 py-10'>
+      <section className='flex flex-col items-center'>
+        <div className='relative flex flex-col items-center'>
+          <div className='absolute left-1/2 top-1/2 z-0 h-[89px] w-[97px] -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white blur-xl' />
+          <Image
+            alt='oph logo'
+            className='z-10'
+            height={89}
+            src='/assets/scan_qr_code/oph-logo.png'
+            width={97}
+          />
+        </div>
+        <div className='mt-2 font-ubuntu text-base font-normal text-white'>
+          CHULA OPENHOUSE 2025
+        </div>
+        <div className='mt-8 flex flex-col items-center gap-1 font-ubuntu'>
+          <div className='text-2xl font-normal text-white'>Welcome!, Staff</div>
+          <div className='text-base font-normal text-white'>username</div>
+        </div>
+        <QrButton />
+      </section>
+
+      <Link className='mt-4' href='/admin/profile/edit'>
+        <Button
+          className='text-md flex h-12 w-72 flex-row items-center justify-center rounded-full bg-white font-normal hover:bg-white/90'
+          size='sm'
+          variant='outline'
+        >
+          <EditSolid className='mr-2' />
+          แก้ไขข้อมูลส่วนตัว
+        </Button>
+      </Link>
     </div>
   )
 }
---
+++ src/components/scan-qr-code/qr-button.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { ScanLine } from 'lucide-react'
+import { useState } from 'react'
+
+import { scanQRCode } from '@/app/actions/qr-code/scan-qr'
+import { useLiffContext } from '@/components/liff/liff-provider'
+
+import Modal from './modal'
+
+type ModalType = 'confirm' | 'invalid' | 'already'
+
+const QrButton: React.FC = () => {
+  const { liff } = useLiffContext()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalType, setModalType] = useState<ModalType>('invalid')
+  const [userInfo, setUserInfo] = useState<string>()
+  const [time, setTime] = useState<string>()
+
+  const handleScan = async (): Promise<void> => {
+    try {
+      if (!liff) {
+        console.error('LIFF is not initialized')
+        return
+      }
+
+      if (!liff.isInClient()) {
+        console.error('Please use this feature inside the LINE app.')
+        return
+      }
+
+      const scanResult = await liff.scanCodeV2()
+      const studentId = scanResult.value
+
+      if (!studentId) {
+        console.error('Failed scanning QR: No studentId')
+        setModalType('invalid')
+        setIsModalOpen(true)
+        return
+      }
+
+      const result = await scanQRCode(studentId)
+
+      if (!result.success) {
+        if (result.error === 'User has already entered') {
+          setModalType('already')
+          setTime(result.lastEntered)
+          setUserInfo(studentId)
+        } else {
+          setModalType('invalid')
+        }
+        setIsModalOpen(true)
+        return
+      }
+
+      setModalType('confirm')
+      setUserInfo(result.data?.name)
+      setTime(result.data?.lastEntered)
+      setIsModalOpen(true)
+    } catch (error) {
+      console.error('Failed to scan QR code:', error)
+      setModalType('invalid')
+      setIsModalOpen(true)
+    }
+  }
+
+  return (
+    <div className='mt-12 flex cursor-pointer justify-center'>
+      <button
+        className='flex h-12 w-72 flex-row items-center justify-center rounded-full bg-white px-4 py-2 text-lg text-dark-pink hover:bg-white/90'
+        type='button'
+        onClick={handleScan}
+      >
+        <ScanLine className='mr-2' size={26} />
+        <span>คลิกเพื่อสแกน</span>
+      </button>
+
+      {isModalOpen ? <Modal
+          closeFn={() => setIsModalOpen(false)}
+          modalType={modalType}
+          scanAgain={handleScan}
+          time={time}
+          userInfo={userInfo}
+        /> : null}
+    </div>
+  )
+}
+
+export default QrButton
