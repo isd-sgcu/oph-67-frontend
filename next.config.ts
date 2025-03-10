b4d3844e6c6e526b46cc5f9bebfd9935b9871c08
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-03-11 06:39:11 +0700

    feat: disable static image

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,13 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_CDN_URL: 'https://cdn.cuopenhouse2025.com',
   },
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.cuopenhouse2025.com/'
+      : '',
+  basePath: '',
   images: {
     domains: ['cdn.cuopenhouse2025.com', 'storage.googleapis.com'],
     remotePatterns: [
@@ -20,6 +26,8 @@ const nextConfig: NextConfig = {
         pathname: '/oph-2025/**',
       },
     ],
+    minimumCacheTTL: 60,
+    disableStaticImages: true,
   },
 }
 
