c251559b3d44e54807ed6aa7f1e86628c390046b
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-03-11 04:33:53 +0700

    feat: cdn

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.cuopenhouse2025.com/'
+      : '',
+  images: {
+    domains: ['cdn.cuopenhouse2025.com', 'storage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.cuopenhouse2025.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/oph-2025/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
