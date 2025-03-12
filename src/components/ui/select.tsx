80711a192227c527fb80781845f3274b2650d3d4
Amphi <6633287021@student.chula.ac.th>
2025-03-12 21:16:50 +0700

    fix: placeholder text color

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,14 +19,17 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-[#82A6A0] data-[placeholder]:text-muted-foreground [&>span]:line-clamp-1',
+      'flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-[#82A6A0] [&>span]:line-clamp-1',
       className
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className='h-4 w-4 opacity-50' />
+      <ChevronDown
+        className='h-4 w-4 opacity-50'
+        style={{ color: '#064E41' }}
+      />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
