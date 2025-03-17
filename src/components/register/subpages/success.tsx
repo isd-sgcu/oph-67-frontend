196331c3bf982c1e1f3c56beae498760679533a2
Amphi <6633287021@student.chula.ac.th>
2025-03-17 23:36:16 +0700

    fix: img full hd

--- src/components/register/subpages/success.tsx
+++ src/components/register/subpages/success.tsx
@@ -23,12 +23,18 @@ const Success: React.FC = () => {
         <div className='h-[300px] w-[300px] rounded-full bg-white blur-2xl' />
       </div>
       <div className='relative z-10 flex flex-col items-center justify-center gap-5'>
-        <Image
-          alt='logo'
-          height={280}
-          src={`${config.cdnURL}/assets/register/oph-logo-nobg.svg`}
-          width={280}
-        />
+        <object
+          className='h-[280px] w-[280px]'
+          data={`${config.cdnURL}/assets/register/oph-logo-nobg.svg`}
+          type='image/svg+xml'
+        >
+          <Image
+            alt='logo'
+            height={280}
+            src={`${config.cdnURL}/assets/register/oph-logo-nobg.svg`}
+            width={280}
+          />
+        </object>
         <div className='flex flex-col items-center justify-center gap-0 font-cloud-soft text-lg font-normal tracking-tight text-[#064E41]'>
           <div className='text-3xl font-bold'>ลงทะเบียนเรียบร้อย!</div>
           <div>อย่าลืมมาเจอกัน</div>
