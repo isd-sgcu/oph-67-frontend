4b7c92dc175260d4a4ba74790653426e277a80c7
Taihenc <51960471+Taihenc@users.noreply.github.com>
2025-04-11 08:20:17 +0700

    feat: redirect from cert to home if can't find evaluaction data

--- src/app/(app)/profile/certificate/page.tsx
+++ src/app/(app)/profile/certificate/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { Toaster, toast } from 'react-hot-toast'
 
+import { CheckEvaluation } from '@/app/actions/evaluation/get-evaluation'
 import { getCertToken } from '@/app/actions/get-cert-token/get-cert-token'
 import { getUser } from '@/app/actions/get-profile/get-user'
 import { useAuth } from '@/components/auth/auth-provider'
@@ -13,39 +16,72 @@ const Certificate: React.FC = () => {
   const [certToken, setCertToken] = useState<string>('')
   const { userId, accessToken, isLoading } = useAuth()
   const [isFetched, setIsFetched] = useState(false)
+  const [isEvaluated, setIsEvaluated] = useState(true)
+  const router = useRouter()
 
   useEffect(() => {
-    async function fetchData(): Promise<void> {
+    const checkEvaluation = async (): Promise<void> => {
       try {
-        console.log(userId, accessToken)
+        if (!userId || !accessToken) {
+          throw new Error('User ID or access token is not available')
+        }
+        const res = await CheckEvaluation(userId)
+        if (res.eval === null) {
+          setIsEvaluated(false)
+          toast.error('โปรดทำแบบประเมินก่อนทำรายการ')
+          return
+        }
+        void fetchData()
+      } catch (error) {
+        if (error instanceof Error) {
+          toast.error(error.message)
+        } else {
+          toast.error('An unexpected error occurred')
+        }
+        console.error('Failed to fetch user data', error)
+      }
+    }
+
+    const fetchData = async (): Promise<void> => {
+      try {
         if (!userId || !accessToken) {
           throw new Error('User ID or access token is not available')
         }
 
         const data = await getUser(userId, accessToken)
-        console.log('User data:', data)
         if (!data.name) {
-          throw new Error('User name is not available')
+          toast.error("Failed to fetch user's name")
+          return
         }
         setUserName(data.name)
         const res = await getCertToken(userId, accessToken)
         setCertToken(res.certToken)
         setIsFetched(true)
       } catch (error) {
+        if (error instanceof Error) {
+          toast.error(error.message)
+        } else {
+          toast.error('An unexpected error occurred')
+        }
         console.error('Failed to fetch user data', error)
       }
     }
     if (userId && accessToken) {
-      void fetchData()
+      void checkEvaluation()
     }
   }, [userId, accessToken]) // Ensure effect runs only when userId is available
 
   if (isLoading) {
     return <LiffLoading />
   }
+  if (!isEvaluated) {
+    // redirect to home if not evaluated
+    router.push('/')
+  }
 
   return (
     <div className='flex h-full w-full grow flex-col items-center gap-3 bg-light-pink pt-8 font-mitr'>
+      <Toaster position='top-center' />
       {isFetched ? (
         <CertificateGenerator token={certToken} userName={userName} />
       ) : (
